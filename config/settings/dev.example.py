32f7d45f4e27274fe8f1824afd79dc451edd7306
Zakir Aghayev <agayevzakir239@gmail.com>
2024-06-23 17:42:38 +0400

    Settings prod and dev configured

--- config/settings/dev.example.py
+++ config/settings/dev.example.py
@@ -1,1 +1,18 @@
 from config.settings.common import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+    "localhost",
+]
--- config/settings/prod.example.py
+++ config/settings/prod.example.py
@@ -1,1 +1,19 @@
 from config.settings.common import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_FILES = os.environ.get('STATIC_FILES', False)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
