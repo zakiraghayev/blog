83e1074d40cc3c8d13fb15accfb27dd71e2f0d94
Zakir Aghayev <agayevzakir239@gmail.com>
2024-06-25 16:44:12 +0400

    dockerfileupdated

--- config/settings/deployment.py
+++ config/settings/deployment.py
@@ -1,17 +1,6 @@
 from config.settings.common import *
 import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
-    }
-}
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
