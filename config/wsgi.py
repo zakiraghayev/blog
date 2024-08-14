b670e745cf35eca310846cb3ecf125d8f3b72c41
zakiraghayev <57976785+zakiraghayev@users.noreply.github.com>
2024-08-14 19:52:22 +0400

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+load_dotenv()  # loads the configs from .env
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'dev')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{ENVIRONMENT}")
 
 application = get_wsgi_application()
